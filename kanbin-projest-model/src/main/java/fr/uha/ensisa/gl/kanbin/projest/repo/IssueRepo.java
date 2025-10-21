eac614363a9f0d7c697fe7ee5658b17c78b66539
will8459 <lliw.tri@gmail.com>
2025-10-21 12:57:35 +0200

    que 2 projets

---
+++ kanbin-projest-model/src/main/java/fr/uha/ensisa/gl/kanbin/projest/repo/IssueRepo.java
@@ -0,0 +1,12 @@
+package fr.uha.ensisa.gl.kanbin.projest.repo;
+
+import java.util.Collection;
+import fr.uha.ensisa.gl.kanbin.projest.Issue;
+
+public interface IssueRepo {
+    public void persist(Issue issue);
+    public void remove(long id);
+    // public Test find(long id);
+    public Collection<Issue> findAll();
+    public long count();
+}
---
+++ kanbin-projest-model/src/main/java/fr/uha/ensisa/gl/kanbin/projest/repo/RepoFactory.java
@@ -0,0 +1,5 @@
+package fr.uha.ensisa.gl.kanbin.projest.repo;
+
+public interface RepoFactory {
+    public IssueRepo getIssueRepo();
+}
