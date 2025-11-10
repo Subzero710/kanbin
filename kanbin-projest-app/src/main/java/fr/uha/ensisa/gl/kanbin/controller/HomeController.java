5b573f82453e9a65f0c09272708f4140ad4da5d5
Sakithyan <sakithyanragavan@gmail.com>
2025-11-10 10:22:40 +0100

    Issue #29 :ajout column

--- kanbin-projest-app/src/main/java/fr/uha/ensisa/gl/kanbin/controller/HomeController.java
+++ kanbin-projest-app/src/main/java/fr/uha/ensisa/gl/kanbin/controller/HomeController.java
@@ -5,6 +5,7 @@ import fr.uha.ensisa.gl.kanbin.projest.model.Column;
 import fr.uha.ensisa.gl.kanbin.projest.repo.BoardRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -37,5 +38,24 @@ public class HomeController {
         // Adds an objet to be used in home.jsp
         ret.addObject("name", name);
         return ret;
+    }
+
+    @PostMapping("/board/add-column")
+    public String addColumn(@RequestParam("title") String title) {
+        // Trouver le Board actif (le premier créé)
+        Board board = boards.findAll().stream().findFirst()
+                .orElseGet(() -> boards.save(new Board("Default")));
+
+        // 2. Créer l'objet Column.
+        String key = title.toLowerCase().replaceAll("\\s+", "-");
+
+        // Le constructeur (key, title) est utilisé. Le BoardRepoMem attribuera l'ID (id=0 initial)
+        Column newColumn = new Column(key, title);
+
+        // 3. Appeler la méthode métier du repository pour ajouter et sauvegarder
+        // Le Dev 1 a inclus cette méthode directement dans son BoardRepoMem.
+        boards.addColumn(board.getId(), newColumn);
+
+        return "redirect:/board";
     }
 }
