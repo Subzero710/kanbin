d95923df3bca54980b21c3fd88865fc34e69400a
Jean <novellojean75@gmail.com>
2025-11-19 12:59:55 +0100

    #32 test column, import in HomeController

--- kanbin-projest-app/src/main/java/fr/uha/ensisa/gl/kanbin/controller/HomeController.java
+++ kanbin-projest-app/src/main/java/fr/uha/ensisa/gl/kanbin/controller/HomeController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import fr.uha.ensisa.gl.kanbin.projest.model.Board;
+import fr.uha.ensisa.gl.kanbin.projest.model.Column;
+import fr.uha.ensisa.gl.kanbin.projest.repo.BoardRepo;
+
 @Controller
 public class HomeController {
     private final BoardRepo boards;
