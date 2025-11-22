81580a05107ab7e574552c024ea478509f705487
EstebanStricker <esteban.stricker@uha.fr>
2025-11-22 16:11:12 +0100

    #23 Suppression des issues par défaut

--- kanbin-projest-app/src/main/java/fr/uha/ensisa/gl/kanbin/controller/IssueController.java
+++ kanbin-projest-app/src/main/java/fr/uha/ensisa/gl/kanbin/controller/IssueController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable; // Import nécessaire pour {id}
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -20,15 +20,6 @@ public class IssueController {
     @Autowired
     public IssueController(IssueRepo issueRepo) {
         this.issueRepo = issueRepo;
-        if (issueRepo.count() == 0) {
-            Issue i1 = new Issue();
-            i1.setTitle("Story: Configurer l'environnement");
-            issueRepo.persist(i1);
-
-            Issue i2 = new Issue();
-            i2.setTitle("Story: Ajouter les tests unitaires");
-            issueRepo.persist(i2);
-        }
     }
 
     @GetMapping("/")
