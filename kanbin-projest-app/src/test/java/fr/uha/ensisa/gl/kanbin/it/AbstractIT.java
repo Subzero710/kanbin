76fdb280c8d23342e672b49b459f8fd02754b5e6
Sakithyan <sakithyanragavan@gmail.com>
2025-12-08 00:27:01 +0100

    Config CI/CD complète : Build, Selenium, Kaniko, K8s+Reparation Bugs

--- kanbin-projest-app/src/test/java/fr/uha/ensisa/gl/kanbin/it/AbstractIT.java
+++ kanbin-projest-app/src/test/java/fr/uha/ensisa/gl/kanbin/it/AbstractIT.java
@@ -15,7 +15,6 @@ import java.time.Duration;
 public abstract class AbstractIT {
 
     protected static WebDriver driver;
-    // Par défaut localhost (pour ton PC), mais surchargé par le CI
     private static String host = "localhost";
     private static String port = "8080";
 
@@ -23,7 +22,7 @@ public abstract class AbstractIT {
     public static void setupWebDriver() throws MalformedURLException {
         if (driver != null) return;
 
-        // 1. Récupération des propriétés (Définies dans le pom.xml ou la ligne de commande)
+        // Récupération des propriétés système injectées par Maven
         String remoteBrowser = System.getProperty("selenium.remote.browser");
         String envHost = System.getProperty("host");
         String envPort = System.getProperty("servlet.port");
@@ -32,23 +31,17 @@ public abstract class AbstractIT {
         if (envHost != null) host = envHost;
         if (envPort != null) port = envPort;
 
-        System.out.println(">>> DEBUG SETUP: Host=" + host + " Port=" + port + " RemoteMode=" + remoteBrowser);
+        System.out.println(">>> DEBUG: Host=" + host + " Port=" + port + " RemoteMode=" + remoteBrowser);
 
         ChromeOptions options = new ChromeOptions();
-        // Options indispensables pour que Chrome ne crash pas dans Docker
-        options.addArguments("--headless=new");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1920,1080");
 
+        // Logique demandée par le prof : si remote.browser est défini, on passe en RemoteWebDriver
         if ("1".equals(remoteBrowser) && remoteUrl != null) {
-            // --- CAS GITLAB CI (Selon le document du prof) ---
-            System.out.println(">>> MODE CI DETECTÉ: Connexion au Selenium distant...");
+            System.out.println(">>> MODE CI: Connexion au Selenium distant...");
             driver = new RemoteWebDriver(new URL(remoteUrl), options);
         } else {
-            // --- CAS LOCAL (Ton PC) ---
-            System.out.println(">>> MODE LOCAL: Démarrage Chrome local...");
+            System.out.println(">>> MODE LOCAL: Démarrage Chrome...");
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver(options);
         }
